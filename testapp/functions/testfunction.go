5021d0a6ef9f4d3e9cf13b2348c9915e9d99f898
dehaayy <dehaayy@gmail.com>
2022-12-15 05:56:38 -0500

    histp

--- testapp/functions/testfunction.go
+++ testapp/functions/testfunction.go
@@ -2,6 +2,11 @@ package lib
 
 import (
 	"bitbucket.org/taubyte/go-sdk/event"
+	"math/rand"
+ 
+    "gonum.org/v1/plot"
+    "gonum.org/v1/plot/plotter"
+    "gonum.org/v1/plot/vg"
 )
 
 //export ping
@@ -11,6 +16,32 @@ func ping(e event.Event) uint32 {
 		return 1
 	}
 
+func histPlot(values plotter.Values) {
+    p, err := plot.New()
+    if err != nil {
+        panic(err)
+    }
+    p.Title.Text = "histogram plot"
+ 
+    hist, err := plotter.NewHist(values, 20)
+    if err != nil {
+        panic(err)
+    }
+    p.Add(hist)
+ 
+    if err := p.Save(3*vg.Inch, 3*vg.Inch, "hist.png"); err != nil {
+        panic(err)
+    }
+}
+
+    var values plotter.Values
+    for i := 0; i < 1000; i++ {
+        values = append(values, rand.NormFloat64())
+    }
+    //boxPlot(values)
+    //barPlot(values[:4])
+    histPlot(values)
+	
 	h.Write([]byte("trial 1"))
 
 	return 0
