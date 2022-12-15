ece6a5547fd17efd910c24167fa467c8306c7da2
dehaayy <dehaayy@gmail.com>
2022-12-15 05:54:15 -0500

    a

--- testapp/functions/testfunction.go
+++ testapp/functions/testfunction.go
@@ -2,9 +2,6 @@ package lib
 
 import (
 	"bitbucket.org/taubyte/go-sdk/event"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
 
 //export ping
@@ -14,31 +11,7 @@ func ping(e event.Event) uint32 {
 		return 1
 	}
 
-	// Create a new plot
-	p, err := plot.New()
-	if err != nil {
-		return 1
-	}
-
-	// Create a new line plotter
-	l, err := plotter.NewLine(plotter.XYs{{0, 0}, {1, 1}, {2, 4}, {3, 9}})
-	if err != nil {
-		return 1
-	}
-
-	// Add the line plotter to the plot
-	p.Add(l)
-
-	// Write the plot to the HTTP response
-	err = p.Write(10*vg.Centimeter, 10*vg.Centimeter, h)
-	if err != nil {
-		return 1
-	}
-
-	// Write the "heyy" text to the HTTP response
-	h.Write([]byte("heyy"))
+	h.Write([]byte("trial 1"))
 
 	return 0
 }
-
-
